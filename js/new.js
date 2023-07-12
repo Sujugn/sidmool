e47e7c198b6ff0fbe4715241afef1fb9519b52de
Sujugn <pepsi030@naver.com>
2023-07-12 20:50:24 +0900

    c

---
+++ js/new.js
@@ -0,0 +1,33 @@
+import newProduct from '../data/product.js';
+
+//pagination
+document.addEventListener(function(){
+    newProduct.addEventListener((function(){
+        
+    }))
+    const productInner = document.querySelector('.product_inner');
+
+    //총페이지수
+    let totalData = newProduct.length;
+    const limit = 12;
+    const totalPage = totalData/limit;
+
+    //현재페이지그룹
+    const pageCount = 5;
+    const currentPage = 1;
+    const pageGroup = Math.ceil(currentPage/pageCount);
+
+    //이전,다음 
+    let lastNumber = pageGroup * pageCount
+
+    if(lastNumber>totalPage){
+        lastNumber=totalPage
+    }
+    
+    let firstNumber = lastNumber - (pageCount - 1);
+
+    const next = lastNumber + 1
+    const prev = firstNumber - 1
+
+
+})
---
+++ js/sub.js
@@ -0,0 +1,12 @@
+
+
+
+$(document).ready(function() {
+$('#more_btn').click(function() {
+  $('#group_inner').toggle();
+});
+});
+
+
+//동적 레이아웃,페이지네이션
+
